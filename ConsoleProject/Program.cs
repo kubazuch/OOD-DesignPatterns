be644ba35f8f13b103baa4bf4d887744c02370a8
KuZu <jakubzuchowski13@gmail.com>
2023-04-22 00:59:27 +0200

    Stage 4 -- hashmap

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -12,68 +12,35 @@ namespace ConsoleProject
         {
             DataManager manager = new DataManager(DataRepresentation.TUPLE_STACK);
 
-            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
-            list.Add("kuzu");
-            list.Add("marcin");
-            list.Add("zosia");
-            list.Add("szymon");
-            list.Add("konrad");
-            list.Add("arek");
-            var iterator = (DoublyLinkedList<string>.DoublyLinkedListIterator)list.GetForwardIterator();
-            Algorithms.Find(iterator, s => s == "kuzu");
-            list.Remove(iterator);
-
-            Console.WriteLine("LinkedList Forward:");
-            var iter = list.GetForwardIterator();
-            while (iter.MoveNext())
+            HashMap<int> map = new (10)
             {
-                Console.WriteLine(iter.Current);
-            }
+                13,
+                4,
+                6,
+                7,
+                1
+            };
 
-            Console.WriteLine("\nLinkedList Reversed:");
-            var iter2 = list.GetReverseIterator();
-            while (iter2.MoveNext())
-            {
-                Console.WriteLine(iter2.Current);
-            }
+            Console.WriteLine("All elements from beginning:");
+            var forwardIter = map.GetForwardIterator();
+            while (forwardIter.MoveNext())
+                Console.WriteLine(forwardIter.Current);
+            Console.WriteLine("All elements from end:");
+            var reverseIter = map.GetReverseIterator();
+            while (reverseIter.MoveNext())
+                Console.WriteLine(reverseIter.Current);
 
-            Vector<string> list2 = new Vector<string>();
-            list2.Add("kuzu");
-            list2.Add("marcin");
-            list2.Add("zosia");
-            list2.Add("szymon");
-            list2.Add("szarcin");
-            list2.Add("szosia");
-            list2.Add("szuzu");
-            list2.Add("arek");
-            list2.Add("konrad");
+            Console.WriteLine("Last even element in map (should be: 6):");
+            Console.WriteLine(Algorithms.Find(map.GetReverseIterator(), x => x % 2 == 0));
 
-            var itera2 = (Vector<string>.VectorIterator)list2.GetForwardIterator();
-            Algorithms.Find(itera2, s => s == "kuzu");
-            list2.Remove(itera2);
+            // Try changing the value
+            Algorithms.ForEach(map.GetForwardIterator(), x => x = x + 1);
 
-            Console.WriteLine("\nVector Forward:");
-            iter = list2.GetForwardIterator();
-            while (iter.MoveNext())
-            {
-                Console.WriteLine(iter.Current);
-            }
-            Console.WriteLine("\nVector Reversed:");
-            iter2 = list2.GetReverseIterator();
-            while (iter2.MoveNext())
-            {
-                Console.WriteLine(iter2.Current);
-            }
+            Console.WriteLine("All elements in map from beginning:");
+            Algorithms.ForEach(map.GetForwardIterator(), Console.WriteLine);
 
-            Console.WriteLine("\nFirst in vector like sz*:");
-            Console.WriteLine(Algorithms.Find(list2, str => str.StartsWith("sz"), true));
-            Console.WriteLine("\nLast in vector like sz*:");
-            Console.WriteLine(Algorithms.Find(list2, str => str.StartsWith("sz"), false));
-
-            Console.WriteLine("\nAll in list with a, from beginning:");
-            Algorithms.Print(list, str => str.Contains("a"), true);
-            Console.WriteLine("\nAll in list with a, from end:");
-            Algorithms.Print(list, str => str.Contains("a"), false);
+            Console.WriteLine("Count of odd numbers:");
+            Console.WriteLine(Algorithms.CountIf(map.GetForwardIterator(), x => x % 2 != 0));
         }
     }
 }
