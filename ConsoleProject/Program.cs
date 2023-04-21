e01d19b83dffe91ab39ac4646e8dd049b217434b
KuZu <jakubzuchowski13@gmail.com>
2023-04-21 22:56:09 +0200

    Stage 2

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -8,11 +8,13 @@ namespace ConsoleProject
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            DataManager manager = new DataManager(DataRepresentation.BASE);
-            manager.PrintData();
+            DataManager manager = new DataManager(DataRepresentation.TUPLE_STACK);
+
+            Console.WriteLine("Zadnie 2: Wypisać linie (numer i nazwa) na których jeżdżą pojazdy kierowane przez kierowców mających przynajmniej 10 lat stażu");
+            foreach (var line in manager.Lines.Where(line => manager.Drivers.Any(d => d.Seniority >= 10 && d.Vehicles.Intersect(line.Vehicles).Any())))
+                Console.WriteLine($"    Line #{line.NumberDec}: {line.CommonName}");
         }
     }
 }
